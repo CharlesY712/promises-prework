9496abd6683d7ec27fd7f43c74894083037f336b
Charles Yach <CharlesY712@gmail.com>
2018-04-28 23:28:31 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Promises Prework
+
+### Complete this lesson on Promises - 
+
+https://gist.github.com/robbiejaeger/dc8f55c1f9462741090862f736b82cab
+
+Deliverable: Read through lesson and lined articles then complete code and questions at the end of the lesson.
---
+++ scripts.js
@@ -0,0 +1,20 @@
+const testNum = (num) => {
+  if (num > 10) {
+    return Promise.resolve(`${num} is greater than 10, success!`);
+  } else if (num === 10) {
+    return Promise.resolve(`${num} is 10, try something else!`);
+  } else {
+    return Promise.reject(`${num} is less than 10, fail!`);
+  }
+};
+
+
+
+// What is .then() used for, and what is .catch() used for?
+// then() is used for triggering the next step after a successful response. catch() is used for triggering the next step after a failed response.
+
+// What are good use cases for Promises?
+// Promises are good for dealing with asyncronous code.
+
+// What other libraries/functions can you find that uses Promises?
+// Battery API, fetch API, ServiceWorker API also use promises.
